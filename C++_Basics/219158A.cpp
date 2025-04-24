a76d4090c862f12132519e5a5d7a69bfb078c649
86pushkar24 <guptapushkar86@gmail.com>
2025-04-25 02:31:08 +0530

    First Problem

---
+++ C++_Basics/219158A.cpp
@@ -0,0 +1,27 @@
+/*
+ *	Author: Mr. PG
+ *	Created: 25.04.2025 02:29:05 (GMT+5:30)
+ */
+#include "bits/stdc++.h"
+using namespace std;
+
+void solve()
+{
+    string s;
+    cin >> s;
+    cout << "Hello, " + s << endl;
+}
+
+signed main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
